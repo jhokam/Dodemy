0df5f9a099e954fe1576f5eca73722998fd5c2fd
hnifalii <ankso30561@gmail.com>
2024-09-07 23:46:39 +0700

    created lot of things :D

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-let hero = document.querySelector("#hero");
-let features = document.querySelector("#features");
+function highlightActiveLink() {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.nav-link');
 
-const sectionAll = document.querySelectorAll("section[id]");
-window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  sectionAll.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop;
-    const sectionID = current.getAttribute("id");
-    console.log(sectionID);
-
-    if (scrollY > sectionTop && sectionHeight <= sectionTop + sectionHeight) {
-      // console.log(document.querySelector('li a[href*="' + sectionID + '"]'))
-      document
-        .querySelector('li a[href="' + sectionID + '"]')
-        .classList.add("active");
+  sections.forEach(section => {
+    const sectionId = section.getAttribute('id');
+    const navLink = document.querySelector(`.nav-link[href="#${sectionId}"]`);
+    if (
+      section.offsetTop <= window.scrollY + 100 &&
+      section.offsetTop + section.offsetHeight > window.scrollY
+    ) {
+      navLink.classList.add('active');
     } else {
-      document
-        .querySelector('li a[href="' + sectionID + '"]')
-        .classList.remove("active");
+      navLink.classList.remove('active');
     }
   });
-});
+}
+
+window.addEventListener('load', highlightActiveLink);
+window.addEventListener('scroll', highlightActiveLink);
