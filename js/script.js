858338c6bb8a689a7a7b1689b9ab6d3f37bd34c8
hnifalii <ankso30561@gmail.com>
2024-09-08 16:48:48 +0700

    created FAQ and footer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+// To highlight nav link when active
 function highlightActiveLink() {
-  const sections = document.querySelectorAll('section');
-  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
 
-  sections.forEach(section => {
-    const sectionId = section.getAttribute('id');
+  sections.forEach((section) => {
+    const sectionId = section.getAttribute("id");
     const navLink = document.querySelector(`.nav-link[href="#${sectionId}"]`);
     if (
       section.offsetTop <= window.scrollY + 100 &&
       section.offsetTop + section.offsetHeight > window.scrollY
     ) {
-      navLink.classList.add('active');
+      navLink.classList.add("active");
     } else {
-      navLink.classList.remove('active');
+      navLink.classList.remove("active");
     }
   });
 }
 
-window.addEventListener('load', highlightActiveLink);
-window.addEventListener('scroll', highlightActiveLink);
+// Trigger highlight function
+window.addEventListener("load", highlightActiveLink);
+window.addEventListener("scroll", highlightActiveLink);
+
+// FAQ dropdown
+function toggleFAQ(button) {
+  const content = button.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+}
